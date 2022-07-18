596d90474807f2c6f2251f6935bb15bd4efc59fd
Camilo Hernandez Cadavid <camilohernandezcadavid@Camilos-MacBook-Pro.local>
2022-07-18 18:17:55 -0500

    downloading files and publishing them in tweets

--- services/create_tweet/service.py
+++ services/create_tweet/service.py
@@ -1,22 +1,44 @@
 import os
+import S3
+import tweepy
 import requests
-from dynamodb import get_items
-from requests_oauthlib import OAuth1
+import dynamodb
 
 def create_tweet(event, context):
   try:
-    items = get_items()
-    message = items['Items'][0]['message']['S']
-    body = {'text': message}
-    url = os.environ['TWITTER_API_URL']
-    auth = OAuth1(os.environ['TWITTER_CONSUMER_KEY'],
-                  os.environ['TWITTER_CONSUMER_SECRET'],
-                  os.environ['TWITTER_ACCESS_TOKEN'],
-                  os.environ['TWITTER_TOKEN_SECRET'])
-    request = requests.post(url, json=body, auth=auth).json()
-    request
+    items = dynamodb.get_items()
+    text = items['Items'][0]['message']['S']
+    images_folder = items['Items'][0]['images_folder']['S']
+
+    S3.download_bot_images(images_folder)
+
+    auth = tweepy.OAuthHandler(
+      os.environ['TWITTER_CONSUMER_KEY'],
+      os.environ['TWITTER_CONSUMER_SECRET']
+    )
+    auth.set_access_token(
+      os.environ['TWITTER_ACCESS_TOKEN'],
+      os.environ['TWITTER_TOKEN_SECRET']
+    )
+    api = tweepy.API(auth)
+    client = tweepy.Client(
+      bearer_token=os.environ['TWITTER_BEARER_TOKEN'],
+      consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+      consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+      access_token=os.environ['TWITTER_ACCESS_TOKEN'],
+      access_token_secret=os.environ['TWITTER_TOKEN_SECRET']
+    )
+
+    images = os.listdir('tmp')
+    media_ids = []
+    for image in images:
+      response = api.media_upload(filename=f'tmp/{image}')
+      media_id = getattr(response, 'media_id_string')
+      media_ids.append(media_id)
+
+    response = client.create_tweet(text=text, media_ids=media_ids)
   except requests.RequestException as e:
     print(e)
-    raise e
+    raise e 
 
-  return request
+  return response
